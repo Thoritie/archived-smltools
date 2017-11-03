81128a36c3da930289344be9a03812b308aab63e
MSI\Poon <poon1412@gmail.com>
2017-11-04 05:32:30 +0700

    not done

--- cache/c__xampp_htdocs_smltools_app_views_index.volt.php
+++ cache/c__xampp_htdocs_smltools_app_views_index.volt.php
@@ -6,15 +6,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Phalcon PHP Framework</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <?= $this->tag->stylesheetLink('bootstrap-4/css/bootstrap.min.css') ?>  
+        <?= $this->tag->stylesheetLink('sml/regis.css') ?>     
+        <!-- <?= $this->tag->stylesheetLink('nav/css/creative-override.css') ?> -->
+        <?= $this->tag->stylesheetLink('nav/css/creative.css') ?>        
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
     </head>
     <body>
         <div class="container">
             <?= $this->getContent() ?>
         </div>
+
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <!-- navbar js -->
+        <?= $this->tag->javascriptInclude('nav/js/creative.js') ?>
     </body>
 </html>
