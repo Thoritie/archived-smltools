e54be25451ff58caef9056405e2cc271f4b37a43
Thoritie <thohirh9@gmail.com>
2017-11-22 15:50:28 +0700

    Task validate so red :wow:

--- public/jquery/createTaskValidate.js
+++ public/jquery/createTaskValidate.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+    $.validator.setDefaults({
+        errorClass: 'badge badge-danger',
+        highlight: function (element) {
+            $(element)
+                .closest('.form-group')
+                .addClass('has-error has-feedback')
+        },
+        unhighlight: function (element) {
+            $(element)
+                .closest('.form-group')
+                .removeClass('has-error has-feedback')
+        }
+    })
     $("#createTask-form").validate({
         rules: {
             taskname: "required",
@@ -17,4 +30,6 @@ $(document).ready(function () {
 
         }
     });
+
+
 });
