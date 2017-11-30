bcd11b7cac7508cbf3af65fc1f6bb6f2358df21e
Thoritie <thohirh9@gmail.com>
2017-11-30 12:26:27 +0700

    resource tag input coming (i forgot s haha) :smile:

--- public/jslif/tagResource.js
+++ public/jslif/tagResource.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    
     function createJSON(data) {
         jsonObj = [];
         $.each(data, function(index ,data){
@@ -35,7 +35,7 @@ $(document).ready(function() {
         Stakeholder.initialize();
 
         var rOwner = $('#rOwner');
-        rOwner.taginput({
+        rOwner.tagsinput({
             itemValue: 'value',
             itemText:'text',
             typeaheadjs : {
@@ -49,19 +49,70 @@ $(document).ready(function() {
         });
     }
 
-    var project = "1";  //input project id later
+    function tagpOwner(n){
+        var Stakeholder = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: JSON.parse(n)
+        });
 
-    $.post("findStake"),{
+        Stakeholder.initialize();
+
+        var pOwner = $('#pOwner');
+        pOwner.tagsinput({
+            itemValue: 'value',
+            itemText:'text',
+            typeaheadjs : {
+                name :'name',
+                displayKey:'text',
+                source : Stakeholder.ttAdapter(),
+                templates :{
+                    empty:'<div class="empty-message text-info"> No matches.</div>'
+                }
+            }
+        });
+    }
+
+    function tagmaintainer(n){
+        var Stakeholder = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: JSON.parse(n)
+        });
+
+        Stakeholder.initialize();
+
+        var maintainer = $('#maintainer');
+        maintainer.tagsinput({
+            itemValue: 'value',
+            itemText:'text',
+            typeaheadjs : {
+                name :'name',
+                displayKey:'text',
+                source : Stakeholder.ttAdapter(),
+                templates :{
+                    empty:'<div class="empty-message text-info"> No matches.</div>'
+                }
+            }
+        });
+    }
+
+
+    var project = "1";  //input project id later
+    
+    $.post("findStake",{
+
         project : project
     }, function(data){
-
+      console.log(data);
         var auto = createJSON(data);
         var n = createString(auto);
 
         tagrOwner(n);
+        tagpOwner(n);
+        tagmaintainer(n);
 
-    }
-
+    },  "json");
 
     // save resource
     $('#saveResource').click(function (){
