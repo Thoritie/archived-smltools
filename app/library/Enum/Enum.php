ae5f357098227f6fed6b296db261724fd9ac09f9
MSI\Poon <poon1412@gmail.com>
2017-12-13 16:21:27 +0700

    commit enum

--- app/library/Enum/Enum.php
+++ app/library/Enum/Enum.php
@@ -3,13 +3,48 @@ namespace Library\Enum;
 
 class Enum
 {
-	const STATUS_ACTIVE = 1;	
-	const STATUS_INACTIVE = 2;
+	const positive = 0;	
+	const neutral = 1;
+	const negative = 2;
 
-	public static $arrStatus = array(
-			self::STATUS_ACTIVE => "เปิดใช้งาน",
-			self::STATUS_INACTIVE => "ปิดใช้งาน",
+	const novice = 0;	
+	const intermediate = 1;
+	const expert = 2;
+
+	const tbd = "?";
+
+	const Onlyone = 0;
+	const One = 1;	
+	const Any = 2;
+	const Group = 3;
+
+	const All = 0;
+	const Some = 1;	
+	
+	
+	public static $attitudeStatus = array(
+			self::positive => "positive",
+			self::neutral => "neutral",
+			self::negative => "negative",
+			self::tbd => "?"
 	);
 
+	public static $domainKnowledgeStatus = array(
+			self::novice => "novice",
+			self::intermediate => "intermediate",
+			self::expert => "expert",
+			self::tbd => "?"
+	);
+	public static $PlayerTypeStatus = array(
+			self::Onlyone => "Onlyone",
+			self::One => "One",
+			self::Any => "Any",
+			self::Group => "Group"
+	);
+	public static $RolePlayerStatus = array(
+			self::All => "All",
+			self::Some => "Some",
+			self::Any => "Any"
+	);
 }
 ?>
